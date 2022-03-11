8133f3c88b7e61ba1a63ca184f91ccf752a0aac5
Hubo <Hoap@qq.com>
2022-03-11 11:46:29 +0800

    添加home基本文件

--- app/app.js
+++ app/app.js
@@ -1,3 +1,13 @@
-window.onload=function(){
-    console.log('test')
-}
+var app = angular.module("myApp", ["ngRoute"]);
+app.controller('myController',['$scope','$location',function($scope,$location){
+    $scope.loginClick = function(){
+        window.alert("登录成功！")
+      console.log($location.url('/')) 
+    }
+}])
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/',{templateUrl:'./pages/home.html'})
+    .when('/login',{templateUrl:'./pages/login.html'})
+    .otherwise({redirectTo:'/login'});
+}]);
